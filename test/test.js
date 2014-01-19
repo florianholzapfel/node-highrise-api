49fcb9bc72ba18b409603ad657f99736044215be
Florian Holzapfel <fholzapfel@gmx.de>
2014-01-19 16:57:42 +0100

    FIX: make code conform to jshint rules

--- test/test.js
+++ test/test.js
@@ -5,7 +5,7 @@
  * Copyright (C) 2013 by Florian Holzapfel
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
+ * of this software and associated documentation files (the 'Software'), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
@@ -14,7 +14,7 @@
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
@@ -26,232 +26,232 @@ var highrise = require('..');
 
 var client = new highrise({
 	secure: true,
-	username: '<COMPANY_NAME>',		// e.g. http://COMPANY_NAME.highrisehq.com
+	username: '<COMPANY_NAME>',	// e.g. http://COMPANY_NAME.highrisehq.com
 	token: '<YOUR_TOKEN>'
 });
 
-client.account.get(function(err, account) {
-	if(err) {
+client.account.get(function (err, account) {
+	if (err) {
 		console.log(err);
 	} else {
-		console.log(JSON.stringify(account, null, "  "));
+		console.log(JSON.stringify(account, null, '  '));
 	}
 });
 
-client.cases.get(true, function(err, cases) {
-	if(err) {
+client.cases.get(true, function (err, cases) {
+	if (err) {
 		console.log(err);
 	} else {
 		console.log(JSON.stringify(cases));
 	}
 });
 
-client.deletions.get(function(err, deletions) {
-	if(err) {
+client.deletions.get(function (err, deletions) {
+	if (err) {
 		console.log(err);
 	} else {
 		console.log(JSON.stringify(deletions));
 	}
 });
 
-client.companies.get(function(err, companies) {
-	if(err) {
+client.companies.get(function (err, companies) {
+	if (err) {
 		console.log(err);
 	} else {
 		console.log(JSON.stringify(companies));
 	}
 });
 
-client.company.get(127058450, function(err, company) {
-	if(err) {
+client.company.get(127058450, function (err, company) {
+	if (err) {
 		console.log(err);
 	} else {
 		console.log(JSON.stringify(company));
 	}
 });
 
-client.people.get(function(err, people) {
-	if(err) {
+client.people.get(function (err, people) {
+	if (err) {
 		console.log(err);
 	} else {
 		console.log(JSON.stringify(people));
 	}
 });
 
-client.person.get(140390490, function(err, person) {
-	if(err) {
+client.person.get(140390490, function (err, person) {
+	if (err) {
 		console.log(err);
 	} else {
 		console.log(JSON.stringify(person));
 	}
 });
 
-client.deals.get(function(err, deals) {
-	if(err) {
+client.deals.get(function (err, deals) {
+	if (err) {
 		console.log(err);
 	} else {
-		console.log(JSON.stringify(deals, null, "  "));
+		console.log(JSON.stringify(deals, null, '  '));
 	}
 });
 
-client.deal.get(2820547, function(err, deal) {
-	if(err) {
+client.deal.get(2820547, function (err, deal) {
+	if (err) {
 		console.log(err);
 	} else {
-		console.log(JSON.stringify(deal, null, "  "));
+		console.log(JSON.stringify(deal, null, '  '));
 	}
 });
 
 client.notes.get({
 	type: 'people',
 	subject: 166222215
-}, function(err, notes) {
-	if(err) {
+}, function (err, notes) {
+	if (err) {
 		console.log(err);
 	} else {
-		console.log(JSON.stringify(notes, null, "  "));
+		console.log(JSON.stringify(notes, null, '  '));
 	}
 });
 
-client.note.get(161143856, function(err, note) {
-	if(err) {
+client.note.get(161143856, function (err, note) {
+	if (err) {
 		console.log(err);
 	} else {
-		console.log(JSON.stringify(note, null, "  "));
+		console.log(JSON.stringify(note, null, '  '));
 		
-		note.comments(function(err, comments) {
-			console.log(JSON.stringify(comments, null, "  "));
+		note.comments(function (err, comments) {
+			console.log(JSON.stringify(comments, null, '  '));
 		});
 	}
 });
 
-client.comment.get(161144101, function(err, comment) {
-	if(err) {
+client.comment.get(161144101, function (err, comment) {
+	if (err) {
 		console.log(err);
 	} else {
-		console.log(JSON.stringify(comment, null, "  "));
+		console.log(JSON.stringify(comment, null, '  '));
 	}
 });
 
-client.categories.get('task', function(err, categories) {
-	if(err) {
+client.categories.get('task', function (err, categories) {
+	if (err) {
 		console.log(err);
 	} else {
-		console.log(JSON.stringify(categories, null, "  "));
+		console.log(JSON.stringify(categories, null, '  '));
 	}
 });
 
-client.category.get('task', 4443591, function(err, category) {
-	if(err) {
+client.category.get('task', 4443591, function (err, category) {
+	if (err) {
 		console.log(err);
 	} else {
-		console.log(JSON.stringify(category, null, "  "));
+		console.log(JSON.stringify(category, null, '  '));
 	}
 });
 
 client.tasks.get({
 	type: 'kases',
 	subject: 915784
-}, function(err, tasks) {
-	if(err) {
+}, function (err, tasks) {
+	if (err) {
 		console.log(err);
 	} else {
-		console.log(JSON.stringify(tasks, null, "  "));
+		console.log(JSON.stringify(tasks, null, '  '));
 	}
 });
 
-client.task.get(29688979, function(err, category) {
-	if(err) {
+client.task.get(29688979, function (err, category) {
+	if (err) {
 		console.log(err);
 	} else {
-		console.log(JSON.stringify(category, null, "  "));
+		console.log(JSON.stringify(category, null, '  '));
 	}
 });
 
-client.users.get(function(err, users) {
-	if(err) {
+client.users.get(function (err, users) {
+	if (err) {
 		console.log(err);
 	} else {
-		console.log(JSON.stringify(users, null, "  "));
+		console.log(JSON.stringify(users, null, '  '));
 	}
 });
 
-client.user.get(905287, function(err, user) {
-	if(err) {
+client.user.get(905287, function (err, user) {
+	if (err) {
 		console.log(err);
 	} else {
-		console.log(JSON.stringify(user, null, "  "));
+		console.log(JSON.stringify(user, null, '  '));
 	}
 });
 
-client.user.me(function(err, user) {
-	if(err) {
+client.user.me(function (err, user) {
+	if (err) {
 		console.log(err);
 	} else {
-		console.log(JSON.stringify(user, null, "  "));
+		console.log(JSON.stringify(user, null, '  '));
 	}
 });
 
-client.groups.get(function(err, groups) {
-	if(err) {
+client.groups.get(function (err, groups) {
+	if (err) {
 		console.log(err);
 	} else {
-		console.log(JSON.stringify(groups, null, "  "));
+		console.log(JSON.stringify(groups, null, '  '));
 	}
 });
 
-client.group.get(479169, function(err, group) {
-	if(err) {
+client.group.get(479169, function (err, group) {
+	if (err) {
 		console.log(err);
 	} else {
-		console.log(JSON.stringify(group, null, "  "));
+		console.log(JSON.stringify(group, null, '  '));
 	}
 });
 
-client.memberships.get(function(err, memberships) {
-	if(err) {
+client.memberships.get(function (err, memberships) {
+	if (err) {
 		console.log(err);
 	} else {
-		console.log(JSON.stringify(memberships, null, "  "));
+		console.log(JSON.stringify(memberships, null, '  '));
 	}
 });
 
-client.membership.get(1596426, function(err, membership) {
-	if(err) {
+client.membership.get(1596426, function (err, membership) {
+	if (err) {
 		console.log(err);
 	} else {
-		console.log(JSON.stringify(membership, null, "  "));
+		console.log(JSON.stringify(membership, null, '  '));
 	}
 });
 
-client.subject_fields.get(function(err, subject_fields) {
-	if(err) {
+client.subject_fields.get(function (err, subject_fields) {
+	if (err) {
 		console.log(err);
 	} else {
-		console.log(JSON.stringify(subject_fields, null, "  "));
+		console.log(JSON.stringify(subject_fields, null, '  '));
 	}
 });
 
-client.parties.deleted(function(err, parties) {
-	if(err) {
+client.parties.deleted(function (err, parties) {
+	if (err) {
 		console.log(err);
 	} else {
-		console.log(JSON.stringify(parties, null, "  "));
+		console.log(JSON.stringify(parties, null, '  '));
 	}
 });
 
-client.parties.search(function(err, parties) {
-	if(err) {
+client.parties.search(function (err, parties) {
+	if (err) {
 		console.log(err);
 	} else {
-		console.log(JSON.stringify(parties, null, "  "));
+		console.log(JSON.stringify(parties, null, '  '));
 	}
 });
 
-client.parties.recently_viewed(function(err, parties) {
-	if(err) {
+client.parties.recently_viewed(function (err, parties) {
+	if (err) {
 		console.log(err);
 	} else {
-		console.log(JSON.stringify(parties, null, "  "));
+		console.log(JSON.stringify(parties, null, '  '));
 	}
 });
